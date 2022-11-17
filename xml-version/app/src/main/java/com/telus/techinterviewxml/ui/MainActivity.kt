ada7ecc406d0dbd1e63403b75e9e31703dcf1cd5
Durini <jcdurini@gmail.com>
2022-11-17 17:05:43 -0600

    Remove hilt libraries and moved activity

---
+++ xml-version/app/src/main/java/com/telus/techinterviewxml/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.telus.techinterviewxml.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
